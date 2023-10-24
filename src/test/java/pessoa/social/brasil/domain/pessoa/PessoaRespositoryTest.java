5d53f373f080170b57033054d26ac6ee190b69e1
jadeilton <jadeiltondm@gmail.com>
2023-10-24 17:28:07 -0300

    feat : Create Class Test PessoaRepositoryTest.

---
+++ src/test/java/pessoa/social/brasil/domain/pessoa/PessoaRespositoryTest.java
@@ -0,0 +1,18 @@
+package pessoa.social.brasil.domain.pessoa;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class PessoaRespositoryTest {
+
+
+
+
+
+}
